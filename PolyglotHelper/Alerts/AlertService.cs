7b0359b163c15b00653b8636339a98aa79732aca
Catherine <catherinesolodoukhina@gmail.com>
2023-08-20 11:45:32 +0200

    Move AlertService and BaseAlertService

---
+++ PolyglotHelper/Alerts/AlertService.cs
@@ -0,0 +1,62 @@
+﻿using PolyglotHelper.Database.Models;
+using PolyglotHelper.Services;
+
+namespace PolyglotHelper.Alerts;
+
+public static class AlertService
+{
+    public static async Task ShowSentenceAlreadyImported(string sentence)
+    {
+        await BaseAlertService.DisplayAlert(
+            "The sentence has been already imported!",
+            $"The sentence '{sentence}' has been already imported!",
+            Responses.Ok);
+    }
+
+    public static async Task<string> ShowTextRequest()
+    {
+        return await BaseAlertService.DisplayPromptAsync(
+            "Enter text",
+            "Please enter text.");
+    }
+
+    public static async Task ShowNoCardsAvailable()
+    {
+        await BaseAlertService.DisplayAlert(
+            "No cards available!",
+            "Please import more for practicing.",
+            Responses.Ok);
+    }
+
+    public static async Task ShowTagWizard()
+    {
+        const string createNew = "Create new";
+        var tags = await TagService.GetAll();
+        var tagStrings = tags.Select(o => o.Tag);
+
+        var result = await BaseAlertService.DisplayActionSheet("Choose tag", createNew, Responses.Cancel, tagStrings.ToArray());
+
+        if (result == Responses.Cancel)
+            return;
+
+        if (result == createNew)
+        {
+            string newTag = await BaseAlertService.DisplayPromptAsync(
+                "Create tag",
+                "Please enter new tag.");
+
+            if (tagStrings.Contains(newTag))
+            {
+                await BaseAlertService.DisplayAlert(
+                    "Error creating tag",
+                    "The tag already exists!",
+                    Responses.Ok);
+            }
+            else
+                TagService.Add(new TagDbItem(newTag));
+        }
+
+        throw new NotImplementedException();
+        System.Diagnostics.Debug.Assert(result == null || result == string.Empty);
+    }
+}
---
+++ PolyglotHelper/Alerts/BaseAlertService.cs
@@ -0,0 +1,31 @@
+﻿namespace PolyglotHelper.Alerts;
+
+public static class BaseAlertService
+{
+    public static void Init(MainPage mainPage)
+    {
+        _mainPage = mainPage;
+    }
+
+    private static MainPage _mainPage;
+
+    public static async Task DisplayAlert(string title, string description, string cancel)
+    {
+        await _mainPage.DisplayAlert(title, description, cancel);
+    }
+
+    public static async Task<string> DisplayActionSheet(string title, string confirm, string cancel)
+    {
+        return await _mainPage.DisplayActionSheet(title, confirm, cancel);
+    }
+
+    public static async Task<string> DisplayActionSheet(string title, string confirm, string cancel, params string[] options)
+    {
+        return await _mainPage.DisplayActionSheet(title, confirm, cancel, options);
+    }
+
+    public static async Task<string> DisplayPromptAsync(string title, string description)
+    {
+        return await _mainPage.DisplayPromptAsync(title, description);
+    }
+}
