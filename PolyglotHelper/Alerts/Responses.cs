eec9773d15f1a6d939692924bcc471134176c653
Catherine <catherinesolodoukhina@gmail.com>
2023-08-20 11:45:46 +0200

    Create Responses class

---
+++ PolyglotHelper/Alerts/Responses.cs
@@ -0,0 +1,9 @@
+﻿namespace PolyglotHelper.Alerts;
+
+public static class Responses
+{
+    public const string Yes = "Yes";
+    public const string No = "No";
+    public const string Ok = "Ok";
+    public const string Cancel = "Cancel";
+}
